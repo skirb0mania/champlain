e0a3fd8479bbc4f26126f1ee37ee14c15f75bff1
Scott Kirby <scottkirby333@gmail.com>
2017-04-22 08:53:58 -0400

    Adding initial draft of user menu framework.

--- main.cpp
+++ main.cpp
@@ -28,10 +28,58 @@ public:
     
 };
 
+//
+// Function Prototypes
+//
+int displayMenu();
+void executeSelection(int selection);
+
+//
+// Main Function
+//
 int main(int argc, const char * argv[]) {
 
-    //Do stuff
-    
+    cout << "\nWelcome to Ticket Manager v0.0" << endl;
+    cout << "==============================" << endl;
+   
+    // In reality, we'll probably call this in a loop
+    // so the user can perform multiple operations  
+    int selection = displayMenu();
+    executeSelection(selection); 
+
     return 0;
 }
 
+//
+// Function Definitions
+//
+int displayMenu() {
+
+    int selection;
+
+    cout << "\nSelect From the Following Menu Items:" << endl;
+    cout << "1. Display Seating Chart" << endl;
+    cout << "2. Request Tickets" << endl;
+    cout << "3. Print Sales Report" << endl;
+    cout << "4. Exit" << endl;
+    cout << "\nEnter the Number of Your Choice: ";
+
+    cin >> selection;
+    return selection;
+}
+
+void executeSelection(int selection) {
+    switch(selection) {
+        case 1:
+            cout << "You selected Display Seating Chart" << endl;
+            break;
+        case 2:
+            cout << "You selected Request Tickets" << endl;
+            break;
+        case 3:
+            cout << "You selected Sales Report" << endl;
+            break;
+        case 4:
+            cout << "Have a Nice Day\n" << endl;
+    } 
+}
